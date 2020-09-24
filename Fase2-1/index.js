92d170490ec8d7bc06c98ab9320c725871d43e14
Sandoque <acsandoque@gmail.com>
2020-09-24 03:58:07 -0300

    primeiro commit

---
+++ Fase2-1/index.js
@@ -0,0 +1,19 @@
+// Criar um programa calcula a média
+// das notas entre os alunos e envia
+//mensagem do cálculo da média
+
+const { type } = require("os")
+
+const Aluno01 = 'Antônio'
+const Aluno02 = "Eduardo"
+const Aluno03 = "Filano"
+const notaAluno01 = 9.8
+const notaAluno02 = 10
+const notaAluno03 = 2
+
+const media = (notaAluno01 + notaAluno02 + notaAluno03) / 3
+
+
+// se a média for maior que 5, parabenize a turma
+
+console.log(media)
